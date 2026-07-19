abbdc5b134c6e184909b12c65d08f180564e9fef
Thomas Meyer <strayfatty@gmail.com>
2026-07-19 20:58:37 +0200

    WIP

--- .config/hypr/modules/exec-once.lua
+++ .config/hypr/modules/exec-once.lua
@@ -5,7 +5,7 @@
 hl.on("hyprland.start", function ()
   hl.exec_cmd("hyprpaper")
   hl.exec_cmd("goxlr-daemon")
-  hl.exec_cmd("waybar")
+  hl.exec_cmd("quickshell")
   hl.exec_cmd("mpd")
   hl.exec_cmd("syncthing")
 end)
